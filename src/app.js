58e7b8d31117603c9c207c91feea4def49a573a6
Miriam Asencio <148205684+Miritzila@users.noreply.github.com>
2024-01-05 10:35:17 +0000

    final

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let quien = ["El perro", "Mi abuela", "La tortuga", "Mi pájaro"];
+  let accion = ["se comió", "orinó en", "aplastó", "rompió"];
+  let que = ["mis deberes", "las llaves", "el coche"];
+  let cuando = [
+    "antes de clase",
+    "justo a tiempo",
+    "cuando acabé",
+    "durante la comida",
+    "mientras rezaba"
+  ];
+
+  function generarExcusa() {
+    let indiceQuien = Math.floor(Math.random() * quien.length);
+    let indiceAccion = Math.floor(Math.random() * accion.length);
+    let indiceQue = Math.floor(Math.random() * que.length);
+    let indiceCuando = Math.floor(Math.random() * cuando.length);
+
+    let excusa =
+      quien[indiceQuien] +
+      " " +
+      accion[indiceAccion] +
+      " " +
+      que[indiceQue] +
+      " " +
+      cuando[indiceCuando] +
+      ".";
+
+    return excusa;
+  }
+
+  // Ejemplo de uso
+  console.log(generarExcusa());
+
+  document.getElementById("excusa").innerText = generarExcusa();
 };
